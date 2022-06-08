26b7d61bb641081bb57589212549b00de43c2941
alina <alina.bregvadze@gmail.com>
2022-06-08 18:15:07 +0400

    add dist folder

---
+++ dist/js/app.0306bc08.js
@@ -0,0 +1,735 @@
+(function () {
+	var t = {
+			6700: function (t, s, e) {
+				var n = {
+					"./af": 3906,
+					"./af.js": 3906,
+					"./ar": 902,
+					"./ar-dz": 3853,
+					"./ar-dz.js": 3853,
+					"./ar-kw": 299,
+					"./ar-kw.js": 299,
+					"./ar-ly": 6825,
+					"./ar-ly.js": 6825,
+					"./ar-ma": 6379,
+					"./ar-ma.js": 6379,
+					"./ar-sa": 7700,
+					"./ar-sa.js": 7700,
+					"./ar-tn": 2059,
+					"./ar-tn.js": 2059,
+					"./ar.js": 902,
+					"./az": 6043,
+					"./az.js": 6043,
+					"./be": 7936,
+					"./be.js": 7936,
+					"./bg": 4078,
+					"./bg.js": 4078,
+					"./bm": 4014,
+					"./bm.js": 4014,
+					"./bn": 9554,
+					"./bn-bd": 7114,
+					"./bn-bd.js": 7114,
+					"./bn.js": 9554,
+					"./bo": 6529,
+					"./bo.js": 6529,
+					"./br": 5437,
+					"./br.js": 5437,
+					"./bs": 9647,
+					"./bs.js": 9647,
+					"./ca": 9951,
+					"./ca.js": 9951,
+					"./cs": 6113,
+					"./cs.js": 6113,
+					"./cv": 7965,
+					"./cv.js": 7965,
+					"./cy": 5858,
+					"./cy.js": 5858,
+					"./da": 3515,
+					"./da.js": 3515,
+					"./de": 2831,
+					"./de-at": 6263,
+					"./de-at.js": 6263,
+					"./de-ch": 2507,
+					"./de-ch.js": 2507,
+					"./de.js": 2831,
+					"./dv": 4510,
+					"./dv.js": 4510,
+					"./el": 8616,
+					"./el.js": 8616,
+					"./en-au": 4595,
+					"./en-au.js": 4595,
+					"./en-ca": 3545,
+					"./en-ca.js": 3545,
+					"./en-gb": 9609,
+					"./en-gb.js": 9609,
+					"./en-ie": 3727,
+					"./en-ie.js": 3727,
+					"./en-il": 3302,
+					"./en-il.js": 3302,
+					"./en-in": 6305,
+					"./en-in.js": 6305,
+					"./en-nz": 9128,
+					"./en-nz.js": 9128,
+					"./en-sg": 4569,
+					"./en-sg.js": 4569,
+					"./eo": 650,
+					"./eo.js": 650,
+					"./es": 6358,
+					"./es-do": 4214,
+					"./es-do.js": 4214,
+					"./es-mx": 8639,
+					"./es-mx.js": 8639,
+					"./es-us": 232,
+					"./es-us.js": 232,
+					"./es.js": 6358,
+					"./et": 7279,
+					"./et.js": 7279,
+					"./eu": 5515,
+					"./eu.js": 5515,
+					"./fa": 7981,
+					"./fa.js": 7981,
+					"./fi": 7090,
+					"./fi.js": 7090,
+					"./fil": 9208,
+					"./fil.js": 9208,
+					"./fo": 2799,
+					"./fo.js": 2799,
+					"./fr": 3463,
+					"./fr-ca": 2213,
+					"./fr-ca.js": 2213,
+					"./fr-ch": 2848,
+					"./fr-ch.js": 2848,
+					"./fr.js": 3463,
+					"./fy": 1468,
+					"./fy.js": 1468,
+					"./ga": 8163,
+					"./ga.js": 8163,
+					"./gd": 2898,
+					"./gd.js": 2898,
+					"./gl": 6312,
+					"./gl.js": 6312,
+					"./gom-deva": 682,
+					"./gom-deva.js": 682,
+					"./gom-latn": 9178,
+					"./gom-latn.js": 9178,
+					"./gu": 5009,
+					"./gu.js": 5009,
+					"./he": 2795,
+					"./he.js": 2795,
+					"./hi": 7009,
+					"./hi.js": 7009,
+					"./hr": 6506,
+					"./hr.js": 6506,
+					"./hu": 9565,
+					"./hu.js": 9565,
+					"./hy-am": 3864,
+					"./hy-am.js": 3864,
+					"./id": 5626,
+					"./id.js": 5626,
+					"./is": 6649,
+					"./is.js": 6649,
+					"./it": 151,
+					"./it-ch": 5348,
+					"./it-ch.js": 5348,
+					"./it.js": 151,
+					"./ja": 9830,
+					"./ja.js": 9830,
+					"./jv": 3751,
+					"./jv.js": 3751,
+					"./ka": 3365,
+					"./ka.js": 3365,
+					"./kk": 5980,
+					"./kk.js": 5980,
+					"./km": 9571,
+					"./km.js": 9571,
+					"./kn": 5880,
+					"./kn.js": 5880,
+					"./ko": 6809,
+					"./ko.js": 6809,
+					"./ku": 6773,
+					"./ku.js": 6773,
+					"./ky": 5505,
+					"./ky.js": 5505,
+					"./lb": 553,
+					"./lb.js": 553,
+					"./lo": 1237,
+					"./lo.js": 1237,
+					"./lt": 1563,
+					"./lt.js": 1563,
+					"./lv": 1057,
+					"./lv.js": 1057,
+					"./me": 6495,
+					"./me.js": 6495,
+					"./mi": 3096,
+					"./mi.js": 3096,
+					"./mk": 3874,
+					"./mk.js": 3874,
+					"./ml": 6055,
+					"./ml.js": 6055,
+					"./mn": 7747,
+					"./mn.js": 7747,
+					"./mr": 7113,
+					"./mr.js": 7113,
+					"./ms": 8687,
+					"./ms-my": 7948,
+					"./ms-my.js": 7948,
+					"./ms.js": 8687,
+					"./mt": 4532,
+					"./mt.js": 4532,
+					"./my": 4655,
+					"./my.js": 4655,
+					"./nb": 6961,
+					"./nb.js": 6961,
+					"./ne": 2512,
+					"./ne.js": 2512,
+					"./nl": 8448,
+					"./nl-be": 2936,
+					"./nl-be.js": 2936,
+					"./nl.js": 8448,
+					"./nn": 9031,
+					"./nn.js": 9031,
+					"./oc-lnc": 5174,
+					"./oc-lnc.js": 5174,
+					"./pa-in": 118,
+					"./pa-in.js": 118,
+					"./pl": 3448,
+					"./pl.js": 3448,
+					"./pt": 3518,
+					"./pt-br": 2447,
+					"./pt-br.js": 2447,
+					"./pt.js": 3518,
+					"./ro": 817,
+					"./ro.js": 817,
+					"./ru": 262,
+					"./ru.js": 262,
+					"./sd": 8990,
+					"./sd.js": 8990,
+					"./se": 3842,
+					"./se.js": 3842,
+					"./si": 7711,
+					"./si.js": 7711,
+					"./sk": 756,
+					"./sk.js": 756,
+					"./sl": 3772,
+					"./sl.js": 3772,
+					"./sq": 6187,
+					"./sq.js": 6187,
+					"./sr": 732,
+					"./sr-cyrl": 5713,
+					"./sr-cyrl.js": 5713,
+					"./sr.js": 732,
+					"./ss": 9455,
+					"./ss.js": 9455,
+					"./sv": 9770,
+					"./sv.js": 9770,
+					"./sw": 959,
+					"./sw.js": 959,
+					"./ta": 6459,
+					"./ta.js": 6459,
+					"./te": 5302,
+					"./te.js": 5302,
+					"./tet": 7975,
+					"./tet.js": 7975,
+					"./tg": 1294,
+					"./tg.js": 1294,
+					"./th": 2385,
+					"./th.js": 2385,
+					"./tk": 4613,
+					"./tk.js": 4613,
+					"./tl-ph": 8668,
+					"./tl-ph.js": 8668,
+					"./tlh": 8190,
+					"./tlh.js": 8190,
+					"./tr": 4506,
+					"./tr.js": 4506,
+					"./tzl": 3440,
+					"./tzl.js": 3440,
+					"./tzm": 9852,
+					"./tzm-latn": 2350,
+					"./tzm-latn.js": 2350,
+					"./tzm.js": 9852,
+					"./ug-cn": 730,
+					"./ug-cn.js": 730,
+					"./uk": 99,
+					"./uk.js": 99,
+					"./ur": 2100,
+					"./ur.js": 2100,
+					"./uz": 6002,
+					"./uz-latn": 6322,
+					"./uz-latn.js": 6322,
+					"./uz.js": 6002,
+					"./vi": 4207,
+					"./vi.js": 4207,
+					"./x-pseudo": 4674,
+					"./x-pseudo.js": 4674,
+					"./yo": 570,
+					"./yo.js": 570,
+					"./zh-cn": 3644,
+					"./zh-cn.js": 3644,
+					"./zh-hk": 2591,
+					"./zh-hk.js": 2591,
+					"./zh-mo": 9503,
+					"./zh-mo.js": 9503,
+					"./zh-tw": 8080,
+					"./zh-tw.js": 8080,
+				};
+				function a(t) {
+					var s = o(t);
+					return e(s);
+				}
+				function o(t) {
+					if (!e.o(n, t)) {
+						var s = new Error("Cannot find module '" + t + "'");
+						throw ((s.code = "MODULE_NOT_FOUND"), s);
+					}
+					return n[t];
+				}
+				(a.keys = function () {
+					return Object.keys(n);
+				}),
+					(a.resolve = o),
+					(t.exports = a),
+					(a.id = 6700);
+			},
+			1127: function (t, s, e) {
+				"use strict";
+				var n = e(9242),
+					a = e(3396);
+				const o = { class: "container" };
+				function r(t, s, e, n, r, i) {
+					const l = (0, a.up)("HeaderComp"),
+						c = (0, a.up)("MainComp");
+					return (
+						(0, a.wg)(),
+						(0, a.iD)(
+							a.HY,
+							null,
+							[(0, a.Wm)(l), (0, a._)("div", o, [(0, a.Wm)(c)])],
+							64
+						)
+					);
+				}
+				const i = { class: "text-center bg-purple-400 text-white p-4 mb-10" },
+					l = (0, a._)(
+						"div",
+						{ class: "text-3xl md:text-5xl font-bold mb-3" },
+						"Covid-19 Tracker",
+						-1
+					),
+					c = (0, a._)(
+						"p",
+						null,
+						[
+							(0, a.Uk)(" API by "),
+							(0, a._)(
+								"a",
+								{
+									class: "text-gray-700",
+									href: "https://covid19api.com",
+									target: "_blank",
+								},
+								"covid19api.com"
+							),
+						],
+						-1
+					),
+					u = [l, c];
+				function j(t, s, e, n, o, r) {
+					return (0, a.wg)(), (0, a.iD)("header", i, u);
+				}
+				var d = { name: "HeaederComp" },
+					m = e(89);
+				const f = (0, m.Z)(d, [["render", j]]);
+				var h = f;
+				const p = { key: 0 },
+					b = {
+						key: 1,
+						class: "flex flex-col align-center justify-center text-center",
+					},
+					g = (0, a._)(
+						"div",
+						{ class: "text-gray-500 text-3xl mt-10 mb-6" },
+						"Fetching Data",
+						-1
+					),
+					v = ["src"];
+				function y(t, s, e, n, o, r) {
+					const i = (0, a.up)("DataTitleComp"),
+						l = (0, a.up)("DataBoxesComp"),
+						c = (0, a.up)("CountrySelectComp");
+					return o.loading
+						? ((0, a.wg)(),
+						  (0, a.iD)("main", b, [
+								g,
+								(0, a._)(
+									"img",
+									{
+										src: o.loadingImg,
+										alt: "loading image",
+										class: "w-40 m-auto",
+									},
+									null,
+									8,
+									v
+								),
+						  ]))
+						: ((0, a.wg)(),
+						  (0, a.iD)("main", p, [
+								(0, a.Wm)(i, { text: o.title, dataDate: o.dataDate }, null, 8, [
+									"text",
+									"dataDate",
+								]),
+								(0, a.Wm)(l, { stats: o.stats }, null, 8, ["stats"]),
+								(0, a.Wm)(
+									c,
+									{ countries: o.countries, onGetCountry: r.getCountryData },
+									null,
+									8,
+									["countries", "onGetCountry"]
+								),
+								o.stats.Country
+									? ((0, a.wg)(),
+									  (0, a.iD)(
+											"button",
+											{
+												key: 0,
+												onClick:
+													s[0] ||
+													(s[0] = (...t) =>
+														r.clearCountry && r.clearCountry(...t)),
+												class:
+													"bg-red-600 text-white rounded p-3 focus:outline-none hover:bg-green-600 mb-10",
+											},
+											" Clear Country "
+									  ))
+									: (0, a.kq)("", !0),
+						  ]));
+				}
+				var x = e(7139);
+				const k = { class: "text-center" },
+					w = { class: "text-3xl font-bold" },
+					C = { class: "text-2xl mt-4 mb-10" };
+				function D(t, s, e, n, o, r) {
+					return (
+						(0, a.wg)(),
+						(0, a.iD)("div", k, [
+							(0, a._)("h2", w, (0, x.zw)(e.text), 1),
+							(0, a._)("div", C, (0, x.zw)(r.timestamp), 1),
+						])
+					);
+				}
+				var z = e(6797),
+					_ = e.n(z),
+					O = {
+						name: " DataTitleComp",
+						props: ["text", "dataDate"],
+						computed: {
+							timestamp: function () {
+								return _()(this.dataDate).format("MMMM Do YYYY, h:mm:ss a");
+							},
+						},
+					};
+				const T = (0, m.Z)(O, [["render", D]]);
+				var M = T;
+				const W = { class: "grid md:grid-cols-2 gap-4" },
+					U = { class: "shadow-md bg-purple-100 p-10 text-center rounded" },
+					G = (0, a._)(
+						"h3",
+						{ class: "text-3xl text-purple-600 font-bold mb-4" },
+						"Cases",
+						-1
+					),
+					N = { class: "text-2xl mb-4" },
+					Y = (0, a._)("span", { class: "font-bold" }, "New:", -1),
+					Z = { class: "text-2xl mb-4" },
+					H = (0, a._)("span", { class: "font-bold" }, "Total:", -1),
+					I = { class: "shadow-md bg-purple-200 p-10 text-center rounded" },
+					S = (0, a._)(
+						"h3",
+						{ class: "text-3xl text-purple-600 font-bold mb-4" },
+						"Deaths",
+						-1
+					),
+					B = { class: "text-2xl mb-4" },
+					q = (0, a._)("span", { class: "font-bold" }, "New:", -1),
+					E = { class: "text-2xl mb-4" },
+					F = (0, a._)("span", { class: "font-bold" }, "Total:", -1);
+				function P(t, s, e, n, o, r) {
+					return (
+						(0, a.wg)(),
+						(0, a.iD)("div", W, [
+							(0, a._)("div", U, [
+								G,
+								(0, a._)("div", N, [
+									Y,
+									(0, a.Uk)(
+										" " + (0, x.zw)(r.numWithCommas(e.stats.NewConfirmed)),
+										1
+									),
+								]),
+								(0, a._)("div", Z, [
+									H,
+									(0, a.Uk)(
+										" " + (0, x.zw)(r.numWithCommas(e.stats.TotalConfirmed)),
+										1
+									),
+								]),
+							]),
+							(0, a._)("div", I, [
+								S,
+								(0, a._)("div", B, [
+									q,
+									(0, a.Uk)(
+										" " + (0, x.zw)(r.numWithCommas(e.stats.NewDeaths)),
+										1
+									),
+								]),
+								(0, a._)("div", E, [
+									F,
+									(0, a.Uk)(
+										" " + (0, x.zw)(r.numWithCommas(e.stats.TotalDeaths)),
+										1
+									),
+								]),
+							]),
+						])
+					);
+				}
+				var A = {
+					name: "dataBoxesComp",
+					props: ["stats"],
+					methods: {
+						numWithCommas(t) {
+							return t.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+						},
+					},
+				};
+				const K = (0, m.Z)(A, [["render", P]]);
+				var L = K;
+				const V = (0, a._)(
+						"option",
+						{ value: "0", selected: "" },
+						"Select Country",
+						-1
+					),
+					$ = ["value"];
+				function J(t, s, e, o, r, i) {
+					return (0, a.wy)(
+						((0, a.wg)(),
+						(0, a.iD)(
+							"select",
+							{
+								onChange: s[0] || (s[0] = (t) => i.onChange()),
+								"onUpdate:modelValue": s[1] || (s[1] = (t) => (r.selected = t)),
+								class:
+									"form-select block w-full border p-3 rounded mt-10 focus:outline-none mb-10",
+							},
+							[
+								V,
+								((0, a.wg)(!0),
+								(0, a.iD)(
+									a.HY,
+									null,
+									(0, a.Ko)(
+										e.countries,
+										(t, s) => (
+											(0, a.wg)(),
+											(0, a.iD)(
+												"option",
+												{ key: s, value: t.ID },
+												(0, x.zw)(t.Country),
+												9,
+												$
+											)
+										)
+									),
+									128
+								)),
+							],
+							544
+						)),
+						[[n.bM, r.selected]]
+					);
+				}
+				var Q = {
+					name: "CountrySelectComp",
+					props: ["countries"],
+					data() {
+						return { selected: 0 };
+					},
+					methods: {
+						onChange() {
+							const t = this.countries.find((t) => t.ID === this.selected);
+							this.$emit("get-country", t);
+						},
+					},
+				};
+				const R = (0, m.Z)(Q, [["render", J]]);
+				var X = R,
+					tt = {
+						name: "MainComp",
+						components: {
+							DataTitleComp: M,
+							DataBoxesComp: L,
+							CountrySelectComp: X,
+						},
+						data() {
+							return {
+								loading: !0,
+								title: "Global",
+								dataDate: "",
+								stats: {},
+								countries: [],
+								loadingImg: e(148),
+							};
+						},
+						methods: {
+							async fetchData() {
+								const t = await fetch("https://api.covid19api.com/summary"),
+									s = await t.json();
+								return s;
+							},
+							getCountryData(t) {
+								(this.stats = t), (this.title = t.Country);
+							},
+							async clearCountry() {
+								(this.loading = !0), (this.title = "Global");
+								const t = await this.fetchData();
+								(this.dataDate = t.Date),
+									(this.stats = t.Global),
+									(this.loading = !1);
+							},
+						},
+						async created() {
+							const t = await this.fetchData();
+							(this.dataDate = t.Date),
+								(this.stats = t.Global),
+								(this.countries = t.Countries),
+								(this.loading = !1);
+						},
+					};
+				const st = (0, m.Z)(tt, [["render", y]]);
+				var et = st,
+					nt = { name: "App", components: { HeaderComp: h, MainComp: et } };
+				const at = (0, m.Z)(nt, [["render", r]]);
+				var ot = at;
+				(0, n.ri)(ot).mount("#app");
+			},
+			148: function (t, s, e) {
+				"use strict";
+				t.exports = e.p + "dist/img/giphy.1031b49d.gif";
+			},
+		},
+		s = {};
+	function e(n) {
+		var a = s[n];
+		if (void 0 !== a) return a.exports;
+		var o = (s[n] = { id: n, loaded: !1, exports: {} });
+		return t[n].call(o.exports, o, o.exports, e), (o.loaded = !0), o.exports;
+	}
+	(e.m = t),
+		(function () {
+			var t = [];
+			e.O = function (s, n, a, o) {
+				if (!n) {
+					var r = 1 / 0;
+					for (u = 0; u < t.length; u++) {
+						(n = t[u][0]), (a = t[u][1]), (o = t[u][2]);
+						for (var i = !0, l = 0; l < n.length; l++)
+							(!1 & o || r >= o) &&
+							Object.keys(e.O).every(function (t) {
+								return e.O[t](n[l]);
+							})
+								? n.splice(l--, 1)
+								: ((i = !1), o < r && (r = o));
+						if (i) {
+							t.splice(u--, 1);
+							var c = a();
+							void 0 !== c && (s = c);
+						}
+					}
+					return s;
+				}
+				o = o || 0;
+				for (var u = t.length; u > 0 && t[u - 1][2] > o; u--) t[u] = t[u - 1];
+				t[u] = [n, a, o];
+			};
+		})(),
+		(function () {
+			e.n = function (t) {
+				var s =
+					t && t.__esModule
+						? function () {
+								return t["default"];
+						  }
+						: function () {
+								return t;
+						  };
+				return e.d(s, { a: s }), s;
+			};
+		})(),
+		(function () {
+			e.d = function (t, s) {
+				for (var n in s)
+					e.o(s, n) &&
+						!e.o(t, n) &&
+						Object.defineProperty(t, n, { enumerable: !0, get: s[n] });
+			};
+		})(),
+		(function () {
+			e.g = (function () {
+				if ("object" === typeof globalThis) return globalThis;
+				try {
+					return this || new Function("return this")();
+				} catch (t) {
+					if ("object" === typeof window) return window;
+				}
+			})();
+		})(),
+		(function () {
+			e.o = function (t, s) {
+				return Object.prototype.hasOwnProperty.call(t, s);
+			};
+		})(),
+		(function () {
+			e.nmd = function (t) {
+				return (t.paths = []), t.children || (t.children = []), t;
+			};
+		})(),
+		(function () {
+			e.p = "/";
+		})(),
+		(function () {
+			var t = { 143: 0 };
+			e.O.j = function (s) {
+				return 0 === t[s];
+			};
+			var s = function (s, n) {
+					var a,
+						o,
+						r = n[0],
+						i = n[1],
+						l = n[2],
+						c = 0;
+					if (
+						r.some(function (s) {
+							return 0 !== t[s];
+						})
+					) {
+						for (a in i) e.o(i, a) && (e.m[a] = i[a]);
+						if (l) var u = l(e);
+					}
+					for (s && s(n); c < r.length; c++)
+						(o = r[c]), e.o(t, o) && t[o] && t[o][0](), (t[o] = 0);
+					return e.O(u);
+				},
+				n = (self["webpackChunkcovid_19_vue"] =
+					self["webpackChunkcovid_19_vue"] || []);
+			n.forEach(s.bind(null, 0)), (n.push = s.bind(null, n.push.bind(n)));
+		})();
+	var n = e.O(void 0, [998], function () {
+		return e(1127);
+	});
+	n = e.O(n);
+})();
+//# sourceMappingURL=app.0306bc08.js.map
